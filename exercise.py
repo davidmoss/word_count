c39f63752f1c304c10cc5a86ca7fa68291d24de8
David Moss <david@vowmade.com>
2019-11-13 17:18:01 +0000

    Processing folder of documents

--- exercise.py
+++ exercise.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 """
 Find the most common occurring words, and the sentences where they are used to
 create the following table:
@@ -14,6 +13,9 @@ philosophy | x, y, z   | I don't have time for *philosophy*
 ---------------------------------------------------
 ---        | ---       | ---
 """
+from collections import defaultdict
+from pathlib import Path
+
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
@@ -33,5 +35,23 @@ def filter_words(words):
     return [stemmer.stem(word) for word in words if word not in stop_words]
 
 
+def populate_map(words, word_map):
+    for word in words:
+        word_map[word] += 1
+
+
+def process_folder(path):
+    word_map = defaultdict(int)
+
+    for file in path.iterdir():
+        if file.is_file():
+            with file.open() as f:
+                words = tokenize_text(f.read())
+                words = filter_words(words)
+                populate_map(words, word_map)
+    return word_map
+
+
 if __name__ == '__main__':
-    get_common_words()
+    path = Path('./test_docs')
+    word_map = process_folder(path)
