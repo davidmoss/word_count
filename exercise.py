19dbae7f2cb2d3d57e52354e25ef087c9d45308a
David Moss <david@vowmade.com>
2019-11-14 17:51:38 +0000

    Adding storage of document names

--- exercise.py
+++ exercise.py
@@ -21,37 +21,86 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
 
 
+class Word():
+    def __init__(self):
+        self.count = 0
+        self.documents = set()
+
+    def __repr__(self):
+        return f'count={self.count} documents={self.documents}'
+
+
 def get_common_words():
     return True
 
 
 def tokenize_text(text):
+    """
+    Tokenizes text string into words
+
+    Parameters:
+    text (str): Body of text to split into words
+
+    Returns:
+    list: Words from text
+    """
     return word_tokenize(text)
 
 
 def filter_words(words):
+    """
+    Transforms words into their shortend form without any stems and filters out
+    stop words
+
+    Parameters:
+    words (list): List of words
+
+    Returns:
+    list: New list of words after filtering
+    """
     stemmer = SnowballStemmer("english")
-    stop_words = set(stopwords.words('english'))
+    stop_words = stopwords.words('english')
     return [stemmer.stem(word) for word in words if word not in stop_words]
 
 
-def populate_map(words, word_map):
+def populate_map(words, word_map, filename):
+    """
+    Populates word_map hashmap with Word objects containing count and occurance
+    or each word
+
+    Parameters:
+    words (list): List of words
+    word_map (dict): Dictionary of unique words
+    filename (str): Of document words have come from
+    """
     for word in words:
-        word_map[word] += 1
+        word_map[word].count += 1
+        word_map[word].documents.add(filename)
 
 
-def process_folder(path):
-    word_map = defaultdict(int)
+def process_folder(folder_path):
+    """
+    Get count of words and their occurance from a folder of text documents
+
+    Parameters:
+    path (str): Path to folder of documents
+
+    Returns:
+    dict: Unique words with their count and occurances
+    """
+    path = Path(folder_path)
+    word_map = defaultdict(Word)
 
     for file in path.iterdir():
         if file.is_file():
             with file.open() as f:
                 words = tokenize_text(f.read())
                 words = filter_words(words)
-                populate_map(words, word_map)
+                populate_map(words, word_map, file.name)
     return word_map
 
 
 if __name__ == '__main__':
-    path = Path('./test_docs')
-    word_map = process_folder(path)
+    world_map = process_folder('./test_docs')
+    import pprint
+    pprint.pprint(world_map)
--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
+
 import pytest
-from exercise import filter_words, get_common_words, tokenize_text
+from exercise import (Word, filter_words, get_common_words, populate_map,
+                      process_folder, tokenize_text)
 
 
 def test_get_common_words():
@@ -14,3 +17,16 @@ def test_tokenize_text():
 def test_filter_words():
     words = ['This', 'is', 'entertaining']
     assert filter_words(words) == ['this', 'entertain']
+
+
+def test_populate_map():
+    word_map = defaultdict(Word)
+    populate_map(['test'], word_map, 'test_doc')
+    assert 'test' in word_map
+    assert word_map['test'].count == 1
+    assert 'test_doc' in word_map['test'].documents
+
+
+def test_process_folder():
+    words = process_folder('./tests/empty')
+    assert words == {}
